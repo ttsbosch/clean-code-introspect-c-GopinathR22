254bb2ed401318355d3c91b66cf302d5b5f0c302
GopinathR22 <68611696+GopinathR22@users.noreply.github.com>
2024-06-13 16:19:57 +0530

    Update TradeProcessor.cpp

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -3,37 +3,6 @@
 #include <string.h>
 #include <errno.h>
 
-char** SplitString(const char* str, char delimiter) {
-    int count = 0;
-    const char* ptr = str;
-    while (*ptr != '\0') {
-        if (*ptr++ == delimiter) {
-            count++;
-        }
-    }
-
-    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
-    int i = 0;
-    ptr = str;
-    char* token = (char*)malloc(strlen(str) + 1);
-    int j = 0;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            token[j] = '\0';
-            tokens[i++] = strdup(token);
-            j = 0;
-        } else {
-            token[j++] = *ptr;
-        }
-        ptr++;
-    }
-    token[j] = '\0';
-    tokens[i++] = strdup(token);
-    tokens[i] = NULL;
-    free(token);
-    return tokens;
-}
-
 void Process(FILE* stream) {
     char line[1024];
     TR objects[1024];
