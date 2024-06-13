6d7072b46a4cbe0a25cdcafa9e8cd517203a46d4
GopinathR22 <68611696+GopinathR22@users.noreply.github.com>
2024-06-13 16:21:18 +0530

    Update TradeProcessor.cpp

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -3,45 +3,45 @@
 #include <string.h>
 #include <errno.h>
 
-void Process(FILE* stream) {
+void convertDataFromCSVtoXML(FILE* InputStream) {
     char line[1024];
-    TR objects[1024];
+    TradeRecord objects[1024];
     int lineCount = 0;
     int objectCount = 0;
 
-    while (fgets(line, sizeof(line), stream)) {
-        char* fields[3];
-        int fieldCount = 0;
+    while (fgets(line, sizeof(line), InputStream)) {
+        char* TradeInfoFields[3];
+        int TotalTradeInfoFields = 0;
         char* token = strtok(line, ",");
         while (token != NULL) {
-            fields[fieldCount++] = token;
+            TradeInfoFields[TotalTradeInfoFields++] = token;
             token = strtok(NULL, ",");
         }
 
-        if (fieldCount != 3) {
-            fprintf(stderr, "WARN: Line %d malformed. Only %d field(s) found.\n", lineCount + 1, fieldCount);
+        if (TotalTradeInfoFields != 3) {
+            fprintf(stderr, "WARN: Line %d malformed. Only %d field(s) found.\n", lineCount + 1, TotalTradeInfoFields);
             continue;
         }
 
-        if (strlen(fields[0]) != 6) {
-            fprintf(stderr, "WARN: Trade currencies on line %d malformed: '%s'\n", lineCount + 1, fields[0]);
+        if (strlen(TradeInfoFields[0]) != 6) {
+            fprintf(stderr, "WARN: Trade currencies on line %d malformed: '%s'\n", lineCount + 1, TradeInfoFields[0]);
             continue;
         }
 
-        int tam;
-        if (!intGetFromString(fields[1], &tam)) {
-            fprintf(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, fields[1]);
+        int tradeamount;
+        if (!tryToConvertasLongInt(TradeInfoFields[1], &tradeamount)) {
+            fprintf(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, TradeInfoFields[1]);
         }
 
-        double tp;
-        if (!toDouble(fields[2], &tp)) {
-            fprintf(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, fields[2]);
+        double tradeprice;
+        if (!tryToConvertasDouble(TradeInfoFields[2], &tradeprice)) {
+            fprintf(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, TradeInfoFields[2]);
         }
 
-        strncpy(objects[objectCount].SC, fields[0], 3);
-        strncpy(objects[objectCount].DC, fields[0] + 3, 3);
-        objects[objectCount].Lots = tam / LotSize;
-        objects[objectCount].Price = tp;
+        strncpy(objects[objectCount].SourceCurrency, TradeInfoFields[0], 3);
+        strncpy(objects[objectCount].DestinationCurrency, TradeInfoFields[0] + 3, 3);
+        objects[objectCount].Lots = tradeamount / LotSize;
+        objects[objectCount].Price = tradeprice;
         objectCount++;
         lineCount++;
     }
@@ -50,8 +50,8 @@ void Process(FILE* stream) {
     fprintf(outFile, "<TradeRecords>\n");
     for (int i = 0; i < objectCount; i++) {
         fprintf(outFile, "\t<TradeRecord>\n");
-        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", objects[i].SC);
-        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", objects[i].DC);
+        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", objects[i].SourceCurrency);
+        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", objects[i].DestinationCurrency);
         fprintf(outFile, "\t\t<Lots>%d</Lots>\n", objects[i].Lots);
         fprintf(outFile, "\t\t<Price>%f</Price>\n", objects[i].Price);
         fprintf(outFile, "\t</TradeRecord>\n");
